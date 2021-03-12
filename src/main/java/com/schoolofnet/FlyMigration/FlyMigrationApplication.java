8919a2bc78320337b295fa8644c150d41ffe8f2e
Leonan Luppi <leonan.luppi@gmail.com>
2021-03-12 16:38:31 -0300

    init

---
+++ src/main/java/com/schoolofnet/FlyMigration/FlyMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.FlyMigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolofnet/FlyMigration/FlyMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.FlyMigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
